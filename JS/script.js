b58607ed6f345fb25a9d17e59f5e63d3f12ebab5
tobenna <tobennaaustin@gmail.com>
2024-03-14 09:27:24 -0100

    first commit

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const aside = document.querySelector("aside");
+
+open.addEventListener('click', () => {
+    // aside.style.width ="70%";
+    aside.style.display = "block";
+    aside.style.transition = ".10s ease"
+  
+  });
+  close.addEventListener('click', () => {
+    // aside.style.width = "0"
+    aside.style.display = "none";
+  
+  });
+  
+  if (window.screen.width < 799) {
+    aside.style.display = "none"
+  }
