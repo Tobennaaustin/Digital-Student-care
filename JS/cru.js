005e2d81206e6deeb54d2b18def8515229af2a1e
tobenna <tobennaaustin@gmail.com>
2024-03-18 15:52:26 -0100

    I made some changes

--- JS/cru.js
+++ JS/cru.js
@@ -4,3 +4,24 @@ const newsletter = document.getElementById('newsletter');
 closeNav.addEventListener('click', () => {
     newsletter.style.display = "none";
 });
+
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('#open')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
